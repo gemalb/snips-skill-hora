2d2b04ac3b09d945eb10c626c85becbd84528ae2
gemalb <54063493+gemalb@users.noreply.github.com>
2019-08-13 17:17:34 +0200

    Afegits arxius
    Arxius inicials

---
+++ action-snips-skill-hora.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from hermes_python.hermes import Hermes 
+import requests
+
+from datetime import datetime
+
+MQTT_IP_ADDR = "localhost" 
+MQTT_PORT = 1883 
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT)) 
+
+
+def extraer_hora():
+    now = datetime.now()
+    if now.hour == 1:
+        sentence = 'Es la una ' + " " + "{0}".format(str(now.minute))
+    else:
+        sentence = 'Son las ' + " " + "{0}".format(str(now.minute))
+
+    sentence = sentence.strip()
+    return sentence
+
+def intent_received(hermes, intent_message):
+    mensaje = extraer_hora()
+    
+    if intent_message.intent.intent_name == 'gemalb:NowTime':
+        hermes.publish_end_session(intent_message.session_id, mensaje)               
+    else:
+        return
+    
+with Hermes(MQTT_ADDR) as h:
+    h.subscribe_intents(intent_received).start()
